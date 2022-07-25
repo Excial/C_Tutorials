bd435d4983589c690e969b4d517052c80e419e12
Excial <98080720+DeclanNgKR@users.noreply.github.com>
2022-07-25 21:29:44 +0800

    Updated with some pointer documents, and char arrays convert to string exercises.

--- Pointers/pointers.c
+++ Pointers/pointers.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 
+void point_function();
+
 int main()
 {
-    int x[5] = {1,2,3,4,5};
-    int* ptr;
+    point_function();
 
-    ptr = &x[2];
-
+    /*
     printf("*ptr = %d\n", *ptr);
     printf("(*ptr+1) = %d\n", *ptr+1);
     printf("(*ptr-1) = %d\n",*ptr-1);
+    */
+
+}
+
+
+void point_function()   {
+
+    int var_x = 100;
+    int var_y = 150;
+    int var_z = 200;
+
+    int * x = &var_x;
+    int * y = &var_y;
+    int * z = &var_z;
+
+    int *ptr1 = &var_x;
+    int *ptr2 = &var_y;
+    int *ptr3 = &var_z;
+
+    printf("%d\n", *ptr1);
+    printf("%p\n", x);
+
+    printf("%d\n", *ptr2);
+    printf("%p\n", y);
+
+    printf("%d\n", *ptr3);
+    printf("%p\n", z);
+
+
+    
 
 }
